8cc67734010d3b4ce90f5c0886f8ccb96c42f41d
jiidapa <jidapa.p@kkumail.com>
2018-06-08 11:44:11 +0700

    eye commit

--- MVCProject/Models/BookProductModel.cs
+++ MVCProject/Models/BookProductModel.cs
@@ -8,8 +8,8 @@ namespace MVCProject.Models
     public class BookProductModel
     {
         public int book_product_id { get; set; }
-        public int book_type_id { get; set; }
-        public int book_category_id { get; set; }
+        public string book_type_id { get; set; }
+        public string book_category_id { get; set; }
         public string book_product_name { get; set; }
         public float book_product_price { get; set; }
         public int book_product_qty { get; set; }
