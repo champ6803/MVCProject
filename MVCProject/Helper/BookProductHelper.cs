dcd019b31c7982fd4cb8d89c0f859609ae9915a5
jiidapa <jidapa.p@kkumail.com>
2018-06-08 15:00:52 +0700

    Eye - edit book product

--- MVCProject/Helper/BookProductHelper.cs
+++ MVCProject/Helper/BookProductHelper.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MVCProject.Models;
+using MVCProject.Libraries;
 
 namespace MVCProject.Helper
 {
     public class BookProductHelper
     {
+        public List<BookProductModel> GetBookProduct()
+        {
+            List<BookProductModel> list = new BookProductLibrary().GetBookProductList();
+            return list;
+        }
+
+        public bool AddBookProduct(BookProductModel bookProd)
+        {
+            return new BookProductLibrary().AddBookProduct(bookProd);
+        }
+
+        internal bool AddBookProduct(BookCategoryModel bookProd)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
