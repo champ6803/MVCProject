72edad798a66c0a61429c78ba0681d84b1be655d
champ <champ6803@gmail.com>
2018-06-15 09:43:50 +0700

    com

---
+++ MVCProject/js/BookCateType/BookCateType.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    getBookCateType();
+});
+
+function getBookCateType()
+{
+    alert('TEst');
+    $.ajax({
+        type: 'GET',
+        url: base_path + 'BookCateType/GetBookCateType',
+        async: false,
+        success: function (data) {
+            if (data) {
+                var x = data;
+            }
+        },
+        error: function (data) {
+            alert('error');
+        }
+    });
+}
